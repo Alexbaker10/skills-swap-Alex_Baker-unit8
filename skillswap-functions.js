7492a736f5dc57974a837819ef6cdd7528ecbcb9
Alexbaker10 <alexwbaker3@gmail.com>
2026-02-15 23:28:34 -0500

    added

--- skillswap-functions.js
+++ skillswap-functions.js
@@ -2,5 +2,7 @@ function filterSkillsByCategory(skills, category) {
   if (category === 'All') return skills;
   return skills.filter(skill => skill.category === category);
 }
-
-module.exports = { filterSkillsByCategory };
+function calculateTotalCost(hourlyRate, hours) {
+  return hourlyRate * hours;
+}
+module.exports = { filterSkillsByCategory, calculateTotalCost };
